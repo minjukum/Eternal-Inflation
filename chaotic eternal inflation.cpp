00e50d78540de8e614641373efe265d127929be6
@minjukum <sarchaan@gmail.com>
2021-01-08 21:23:52 +0900

    trial 1

--- chaotic eternal inflation.cpp
+++ chaotic eternal inflation.cpp
@@ -5,6 +5,10 @@ chaotic potentials, V(phi)=lambda*(phi^n).
 Natural units are used; c = hbar = 1
 Phi, V(phi) values are normalized with the reduced Plack mass;
  m_Planck = 1/sqrt(8*pi*G)
+
+It traces the evolution of phi, counting the number of runs
+that exits the inflation.
+
 */
 
 #include <iostream>
@@ -19,34 +23,39 @@ using namespace std;
 
 ofstream outfile;
 void writefile(double, double);
-double rng_constant();
+double rng_uniform();
 double rng_gaussian(double);
 
 int int main(int argc, char const *argv[]) {
   //variables
-  long int n_pat, n_step, n;
+  long int n_run, n_step, n, n_exit;
   double t, dt;
   double phi, dphi_cl, dphi_qu, phi_max, phi_min;
-  double V, V_prime;
+  double lambda, V, V_prime;
   double H;
   clock_t start, finish;
 
-  // FILENAME SETTING
-  outfile.open("")
+  //read in # of iterations, # of timesteps per iteration, chaotic potential exponent
+  cout << "enter # of iterations, # of timesteps per run, exponent n" << endl;
+  cin >> n_run >> n_step >> n;
 
-  //read in # of runs, # of steps per run, chaotic potential exponent
-  cout << "enter # of patches, # of steps, exponent n" << endl;
-  cin >> n_pat >> n_step >> n;
+  //filename setting
+  char* filename = new char[100];
+  sprintf(filename, "n=%ld potential w %ld iterations.txt", n, n_run);
+  outfile.open(filename);
 
-  //calculate the constant lambda
-  lambda = //A FUNCTION OF n
+  //set constants
+  n_exit = 0;
+  phi_max = sqrt(8*M_PI);
+  phi_min = n/sqrt(2);
+  lambda = pow(2*M_PI,2)*3*(n**2)/pow((120*n + n*n/2),1+n/2)*2.1*pow(10,-9);
 
   start = clock();
 
-  //patch loop
-  for (int i = 0; i < n_patch; i++) {
+  //run loop
+  for (int i = 0; i < n_run; i++) {
     t = 0;
-    phi = rng_constant();       //get phi_initial
+    phi = rng_uniform();       //get phi_initial
     V = lambda*exp(phi,n);
     V_prime = n*lambda*exp(phi,n-1);
     H = sqrt(V/3);
@@ -54,7 +63,7 @@ int int main(int argc, char const *argv[]) {
 
     writefile(t,phi);
 
-    //evolution for each patch
+    //evolution for each run
     for (int j = 0; j < n_step; j++) {
       t += dt;
       //calculate the next phi
@@ -69,7 +78,8 @@ int int main(int argc, char const *argv[]) {
 
       //check if the reheating condition is met
       if (phi < phi_min) {
-        //HALT AND QUIT
+        n_exit++;
+        break;
       }
 
       //calculate V(phi), H(phi)
@@ -82,9 +92,14 @@ int int main(int argc, char const *argv[]) {
 
     outfile << endl;
 
-  } //patch loop
+  } //run loop
 
   finish = clock();
+  outfile.close
+
+  cout << "Done! It took " << (float(finish-start)/CLOCKS_PER_SEC) << " seconds." << endl;
+  cout << sprintf("%ld out of %ld exited inflation.",n_exit,n_run) << endl;
+  cout << "For more information, see the generated .txt file." << endl;
 
   return 0;
 }
@@ -97,11 +112,11 @@ void writefile(double t, double phi)
 }
 
 //random number generator with a uniform real distribution
-double rng_constant()
+double rng_uniform()
 {
   int seed = chrono::system_clock::now().time_since_epoch().count();
   mt19937_64 generator(seed);
-  uniform_real_distribution<double> distribution(0.,1.);
+  uniform_real_distribution<double> distribution(0.,sqrt(8*M_PI));
   double phi = distribution(generator);
   return phi;
 }
